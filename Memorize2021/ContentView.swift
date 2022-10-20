7e62efdb179b8aadc4ec299712daf499c4d87f35
GnadtFernando <gnadtfernando45@gmail.com>
2022-10-20 13:23:52 -0300

    Lecture 2

--- Memorize2021/ContentView.swift
+++ Memorize2021/ContentView.swift
@@ -8,28 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var emojis = ["🛴","✈️","🚜","a","c","x","y","k","t","f","j","0","5"]
+    @State var emojiCount = 10
+    
     var body: some View {
-        HStack{
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack{
+            ScrollView{
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]){
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack{
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
-        .foregroundColor(.red)
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1{
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+               
+        }
+    }
+    var add: some View{
+        Button  {
+            if emojiCount < emojis.count{
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+                
+        }
     }
 }
 
 struct CardView: View {
     @State var isFaceUp: Bool = true
+    var content: String
     
     var body: some View {
         ZStack {
             let shape =  RoundedRectangle(cornerRadius: 25.0)
             if isFaceUp{
                 shape.fill().foregroundColor(.white)
-                shape.stroke(lineWidth: 3)
-                Text("🛴").font(.largeTitle)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
             } else {
                shape.fill()
                 }
